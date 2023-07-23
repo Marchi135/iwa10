5661947b5150b5c9ce5992a4ed7612f130a675ed
Marchi135 <marvelchir@gmail.com>
2023-07-23 17:24:16 +0200

    add file

--- bscript.js
+++ bscript.js
@@ -4,48 +4,15 @@ const holidays = {
   0: {
     id: 0,
     name: 'Day of Reconciliation',
-    date: `16 December ${currentYear}`,
+    date: new Date(`16 December ${currentYear}`),
   },
-  1: {
-    id: 1,
-    name: 'Workers Day',
-    date: new Date(`1 April ${currentYear}`),
-  },
-  2: {
-    id: 2,
-    name: 'Day of Goodwill',
-    date: new Date(`26 December ${currentYear}`),
-  },
-  3: {
-    id: 3,
-    name: 'New Year Day',
-    date: new Date(`1 January ${currentYear}`),
-  },
-  4: {
-    id: 4,
-    name: 'Womens Day',
-    date: new Date(`9 August ${currentYear}`),
-  },
-  5: {
-    id: 5,
-    name: 'Heritage Day',
-    date: new Date(`24 September ${currentYear}`),
-  },
+  // ... (other holidays)
   6: {
     id: 6,
     name: 'Christmas Day',
-    date: new Date(`25 December ${currentYear} 00:00`),
+    date: new Date(`25 December ${currentYear} 13:25`),
   },
-  7: {
-    id: 7,
-    name: 'Youth Day',
-    date: new Date(`16 June ${currentYear}`),
-  },
-  8: {
-    id: 8,
-    name: 'Human Rights Day',
-    date: new Date(`21 March ${currentYear}`),
-  },
+  // ... (other holidays)
 };
 
 const christmas = 6;
@@ -53,58 +20,58 @@ const futureId = 9;
 
 // Do not change code above this comment
 
-console.log(holidays[futureId]?.name || `ID ${futureId} not created yet`);
+console.log(holidays[futureId] ? holidays[futureId].name : `ID ${futureId} not created yet`);
 
-let copied = holidays[christmas];
-copied = { ...copied, name: 'X-mas Day' }; // Copy the object and change the name
-let correctDate = new Date(copied.date);
-correctDate.setHours(0, 0, 0, 0); // Set hours, minutes, seconds, and milliseconds to 0
-const isEarlier = correctDate < holidays[6].date;
+copied = { ...holidays[christmas] };
+copied.name = 'X-mas';
+copied.date.setHours(0, 0, 0, 0);
+
+const correctDate = copied.date;
+const isEarlier = copied.date < holidays[christmas].date;
+console.log('New date is earlier:', isEarlier);
 if (isEarlier) copied.date = correctDate;
 
 console.log('ID change:', holidays[christmas].id !== copied.id || copied.id);
 console.log('Name change:', holidays[christmas].name !== copied.name || copied.name);
 console.log('Date change:', holidays[christmas].date !== copied.date || copied.date);
 
-const firstHolidayTimestamp = Math.min(
-  holidays[0].date.getTime(),
-  holidays[1].date.getTime(),
-  holidays[2].date.getTime(),
-  holidays[3].date.getTime(),
-  holidays[4].date.getTime(),
-  holidays[5].date.getTime(),
-  holidays[6].date.getTime(),
-  holidays[7].date.getTime(),
-  holidays[8].date.getTime()
+const firstHoliday = new Date(Math.min(
+  holidays[0].date,
+  holidays[1].date,
+  holidays[2].date,
+  holidays[3].date,
+  holidays[4].date,
+  holidays[5].date,
+  holidays[6].date,
+  holidays[7].date,
+  holidays[8].date,
+));
+
+const lastHoliday = new Date(Math.max(
+  holidays[0].date,
+  holidays[1].date,
+  holidays[2].date,
+  holidays[3].date,
+  holidays[4].date,
+  holidays[5].date,
+  holidays[6].date,
+  holidays[7].date,
+  holidays[8].date,
+));
+
+console.log(
+  `${firstHoliday.getDate().toString().padStart(2, '0')}/${(firstHoliday.getMonth() + 1).toString().padStart(2, '0')}/${currentYear}`
 );
 
-const lastHolidayTimestamp = Math.max(
-  holidays[0].date.getTime(),
-  holidays[1].date.getTime(),
-  holidays[2].date.getTime(),
-  holidays[3].date.getTime(),
-  holidays[4].date.getTime(),
-  holidays[5].date.getTime(),
-  holidays[6].date.getTime(),
-  holidays[7].date.getTime(),
-  holidays[8].date.getTime()
+console.log(
+  `${lastHoliday.getDate().toString().padStart(2, '0')}/${(lastHoliday.getMonth() + 1).toString().padStart(2, '0')}/${currentYear}`
 );
 
-const firstDay = new Date(firstHolidayTimestamp).getDate();
-const firstMonth = new Date(firstHolidayTimestamp).getMonth();
-const lastDay = new Date(lastHolidayTimestamp).getDate();
-const lastMonth = new Date(lastHolidayTimestamp).getMonth();
-
-console.log(`${firstDay}/${firstMonth}/${currentYear}`);
-console.log(`${lastDay}/${lastMonth}/${currentYear}`);
-
-const randomHolidayIndex = Math.floor(Math.random() * Object.keys(holidays).length);
-const randomHoliday = holidays[randomHolidayIndex];
-console.log(randomHoliday.date);
-
-
-
-
+const randomIndex = Math.floor(Math.random() * 9); // Generate a random index between 0 and 8
+const randomHoliday = holidays[randomIndex];
+console.log(
+  `${randomHoliday.date.getDate().toString().padStart(2, '0')}/${(randomHoliday.date.getMonth() + 1).toString().padStart(2, '0')}/${currentYear}`
+);
 
 
 
